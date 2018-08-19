6d396583b873ed96fddd39c2985778684fe09889
thili-hans <hansamalthilina@gmail.com>
2018-08-19 19:34:40 +0530

    test - thilina

---
+++ Thili-Hans/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+ <title> WELCOME </title>
+ <link href="bootstrap/css/bootstrap.css" rel="stylesheet"/>
+</head>
+<body>
+    <?php
+        $x = 100;
+        echo "<div>" . $x . "</div>";// div block of element
+        echo "<div>" . $x * 2 . "</div>";
+        ?>
+    
+</body>
+</html> 
